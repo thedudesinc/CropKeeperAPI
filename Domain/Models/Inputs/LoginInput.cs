02183d7336bc98bbb28eff09d0ca6904b04a2a9b
Zachary Pruessner <zachpruessner@gmail.com>
2023-04-05 21:58:25 -0500

    added validation control

--- Domain/Models/Inputs/LoginInput.cs
+++ Domain/Models/Inputs/LoginInput.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CropKeeperApi.Domain.Models.Inputs;
 
 public class LoginInput
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
